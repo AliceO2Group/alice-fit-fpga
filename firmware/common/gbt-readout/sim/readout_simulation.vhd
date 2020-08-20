fe82e73e1e7804c06f0030dce7ff54467842382b
Dmitry Finogeev <dmitry-finogeev@yandex.ru>
2020-08-20 12:28:35 +0300

    simulation launched on PM, seems ok

--- firmware/common/gbt-readout/sim/readout_simulation.vhd
+++ firmware/common/gbt-readout/sim/readout_simulation.vhd
@@ -44,10 +44,7 @@ ARCHITECTURE behavior OF testbench_readout IS
 
    -- inputs file --------------------------------------
    file input_reg_file : text open read_mode is "..\..\..\..\..\..\..\..\software\readout-sim\simulation_inputs\simple_sig_inputs.txt";
-   --file input_reg_file : text open read_mode is "C:\Vivado_projects\alice-fit-fpga\firmware\common\gbt-readout\sim\inputs_test.txt";
-   --file input_reg_file : text open read_mode is "C:\Vivado_projects\alice-fit-fpga\software\readout-sim\simulation_inputs\simple_sig_inputs.txt";
-   file output_rd_file : text open write_mode is "gbt_out.txt";
-   --file input_reg_file : text open read_mode is "inputs_test.txt";
+   file output_rd_file : text open write_mode is "..\..\..\..\..\..\..\..\software\readout-sim\simulation_outputs\readout_gbt_output.txt";
    signal Control_register_from_file : cntr_reg_addrreg_type;
    -- ---------------------------------------------------
 
@@ -57,7 +54,7 @@ ARCHITECTURE behavior OF testbench_readout IS
 	constant ipbus_clock_period : time := 33.333 ns;
 	signal RESET : std_logic := '0';
 	signal SYS_CLK : std_logic := '0';
-	signal DATA_CLK : std_logic := '0';
+	signal DATA_CLK, DATA_CLK_ff : std_logic := '0';
 	signal IPBUS_CLK : std_logic := '0';
 	signal GBT_RxFrameClk : std_logic := '0';
 	
@@ -185,16 +182,15 @@ Sys1_process :process
    variable was_reset : integer := 0;
    variable counter : integer := 0;
    
-   -- file data ------------------
-   constant infile_num_col : integer := cntr_reg_n_32word*2;
-   variable infile_line : line;
-   variable outfile_line   : line;
-   type infile_data_type is array (integer range <>) of integer;
+--   -- file data ------------------
+--   constant infile_num_col : integer := cntr_reg_n_32word*2;
+--   variable infile_line : line;
+--   variable outfile_line   : line;
+--   type infile_data_type is array (integer range <>) of integer;
    
-   variable data_from_file : infile_data_type(0 to infile_num_col-1);
-   variable datavec_from_file : cntr_reg_addrreg_type;
-
-   -- -----------------------------
+--   variable data_from_file : infile_data_type(0 to infile_num_col-1);
+--   variable datavec_from_file : cntr_reg_addrreg_type;
+--   -- -----------------------------
    
    begin
    
@@ -202,7 +198,7 @@ Sys1_process :process
 			was_reset := was_reset + 1;
 			RESET <= '1';
 			
-			data_from_file := (others=>0);
+--			data_from_file := (others=>0);
 		else
 			RESET <= '0';
 		end if;
@@ -214,30 +210,33 @@ Sys1_process :process
 		
 		if(counter <= 4) then DATA_CLK <= '0'; else 
 		  DATA_CLK <= '1';
+		end if;
+		
+--		DATA_CLK_ff <= DATA_CLK;
+--		if (DATA_CLK = '1') and (DATA_CLK_ff = '0') then
+--		  if(not endfile(input_reg_file)) then
+--	   	       readline(input_reg_file, infile_line);
+--		  end if;
+--		  for irow in 0 to infile_num_col-1 loop
+--		      read(infile_line, data_from_file(irow));
+--		      --hread(infile_line, datavec_from_file(irow));
+--		      --Control_register_from_file(irow) <= std_logic_vector(to_unsigned(data_from_file(irow),32));
+--		  end loop;
+--		  for irow in 0 to cntr_reg_n_32word-1 loop
+--		      Control_register_from_file(irow)(15 downto 0) <= std_logic_vector(to_unsigned(data_from_file(irow*2+1),16));
+--		      Control_register_from_file(irow)(31 downto 16) <= std_logic_vector(to_unsigned(data_from_file(irow*2),16));
+--		  end loop;
+--		  --Control_register_from_file <= datavec_from_file;
 		  
-		  if(not endfile(input_reg_file)) then
-	   	       readline(input_reg_file, infile_line);
-		  end if;
-		  for irow in 0 to infile_num_col-1 loop
-		      read(infile_line, data_from_file(irow));
-		      --hread(infile_line, datavec_from_file(irow));
-		      --Control_register_from_file(irow) <= std_logic_vector(to_unsigned(data_from_file(irow),32));
-		  end loop;
-		  for irow in 0 to cntr_reg_n_32word-1 loop
-		      Control_register_from_file(irow)(15 downto 0) <= std_logic_vector(to_unsigned(data_from_file(irow*2+1),16));
-		      Control_register_from_file(irow)(31 downto 16) <= std_logic_vector(to_unsigned(data_from_file(irow*2),16));
-		  end loop;
-		  --Control_register_from_file <= datavec_from_file;
+--		  if IsData_from_FITrd = '1' then
+--		      hwrite(outfile_line, Data_from_FITrd);
+--		      writeline(output_rd_file, outfile_line);
+--		  end if;
 		  
-		  if IsData_from_FITrd = '1' then
-		      hwrite(outfile_line, Data_from_FITrd);
-		      writeline(output_rd_file, outfile_line);
-		  end if;
+--		  testbench_CONTROL_REG_dynamic <= func_CNTRREG_getcntrreg(Control_register_from_file);
+--		 -- testbench_CONTROL_REG_dynamic <= func_CNTRREG_getcntrreg(datavec_from_file);
 		  
-		  testbench_CONTROL_REG_dynamic <= func_CNTRREG_getcntrreg(Control_register_from_file);
-		 -- testbench_CONTROL_REG_dynamic <= func_CNTRREG_getcntrreg(datavec_from_file);
-		  
-	    end if;
+--	    end if;
 		  
 		if(counter = 8) then counter := 0; end if;
 		
@@ -302,6 +301,55 @@ Sys2_process :process
     
   end process;
 -- =====================================================
+
+
+
+-- Data ff data clk ***********************************
+	PROCESS (FSM_Clocks_signal.Data_Clk)
+       -- file data ------------------
+       constant infile_num_col : integer := cntr_reg_n_32word*2;
+       variable infile_line : line;
+       variable outfile_line : line;
+       type infile_data_type is array (integer range <>) of integer;
+       
+       variable data_from_file : infile_data_type(0 to infile_num_col-1);
+       variable datavec_from_file : cntr_reg_addrreg_type;
+       -- -----------------------------
+        BEGIN
+		IF(FSM_Clocks_signal.Data_Clk'EVENT and FSM_Clocks_signal.Data_Clk = '1') THEN
+			IF(FSM_Clocks_signal.Reset = '1') THEN
+                    data_from_file := (others=>0);
+			ELSE
+                if (DATA_CLK = '1') and (DATA_CLK_ff = '0') then
+                  if(not endfile(input_reg_file)) then
+                       readline(input_reg_file, infile_line);
+                  end if;
+                  for irow in 0 to infile_num_col-1 loop
+                      read(infile_line, data_from_file(irow));
+                      --hread(infile_line, datavec_from_file(irow));
+                      --Control_register_from_file(irow) <= std_logic_vector(to_unsigned(data_from_file(irow),32));
+                  end loop;
+                  for irow in 0 to cntr_reg_n_32word-1 loop
+                      Control_register_from_file(irow)(15 downto 0) <= std_logic_vector(to_unsigned(data_from_file(irow*2+1),16));
+                      Control_register_from_file(irow)(31 downto 16) <= std_logic_vector(to_unsigned(data_from_file(irow*2),16));
+                  end loop;
+                  --Control_register_from_file <= datavec_from_file;
+                  
+                  if (IsData_from_FITrd = '1') then
+                      hwrite(outfile_line, Data_from_FITrd);
+                      writeline(output_rd_file, outfile_line);
+                  end if;
+                  
+                  testbench_CONTROL_REG_dynamic <= func_CNTRREG_getcntrreg(Control_register_from_file);
+	    end if;
+			END IF;
+			
+			
+		END IF;
+		
+		
+	END PROCESS;
+-- ****************************************************
 
 
 
