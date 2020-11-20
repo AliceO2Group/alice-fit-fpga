50ffb385277fcf964efbe43e410265a182884288
Dmitry Finogeev <dmitry-finogeev@yandex.ru>
2020-11-20 18:27:35 +0300

    couple of data could be dropped after SOr, it is fine

--- software/readout-sim/simple_inputs.py
+++ software/readout-sim/simple_inputs.py
@@ -15,7 +15,7 @@ sigin_file = open('simulation_inputs/simple_sig_inputs.txt', 'w')
 # generators setup ============================
 control_reg.data_gen = cntrl_reg.gen_mode.main_gen
 control_reg.data_trg_respond_mask = 0
-control_reg.data_bunch_pattern = 0x11111111
+control_reg.data_bunch_pattern = 0x0
 control_reg.data_bunch_freq = 0xdec
 control_reg.data_freq_offset = 0xdeb-5
 control_reg.trg_gen = cntrl_reg.gen_mode.main_gen
@@ -48,17 +48,20 @@ for i in range(2*0xdec):
     sigin_file.write(control_reg.get_reg_line_16() + '\n')
 
 
-
+# start empty cycle ===========================
+for i in range(1*0xdec):
+    sigin_file.write(control_reg.get_reg_line_16() + '\n')
 # continious run ===========================
 print("continious run =================== ")
 control_reg.trg_rd_command = cntrl_reg.readout_cmd.continious
-for i in range(5*0xdec):
+for i in range(1*0xdec):
     sigin_file.write(control_reg.get_reg_line_16() + '\n')
-
-
-
+control_reg.data_bunch_pattern = 0x11111111
+for i in range(6*0xdec):
+    sigin_file.write(control_reg.get_reg_line_16() + '\n')
 # end empty cycle ===========================
 control_reg.trg_rd_command = cntrl_reg.readout_cmd.idle
+control_reg.data_bunch_pattern = 0x0
 for i in range(2*0xdec):
     sigin_file.write(control_reg.get_reg_line_16() + '\n')
 
@@ -67,17 +70,14 @@ for i in range(2*0xdec):
 # start empty cycle ===========================
 for i in range(2*0xdec):
     sigin_file.write(control_reg.get_reg_line_16() + '\n')
-
-
-
 # trigger run ===========================
 print("trigger run =================== ")
 control_reg.trg_rd_command = cntrl_reg.readout_cmd.trigger
-for i in range(5*0xdec):
+for i in range(1*0xdec):
     sigin_file.write(control_reg.get_reg_line_16() + '\n')
-
-
-
+control_reg.data_bunch_pattern = 0x11111111
+for i in range(6*0xdec):
+    sigin_file.write(control_reg.get_reg_line_16() + '\n')
 # end empty cycle ===========================
 control_reg.trg_rd_command = cntrl_reg.readout_cmd.idle
 for i in range(2*0xdec):
