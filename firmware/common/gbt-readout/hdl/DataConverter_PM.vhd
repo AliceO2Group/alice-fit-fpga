da153990e776f52e700c1a94fca623d649539a85
Dmitry Finogeev <d.finogeev@gsi.de>
2021-07-13 12:59:34 +0300

    submodules removed
    submodules folder was removed as unused.
    gbt-fpga is used from common/gbt-fpga

--- firmware/common/gbt-readout/hdl/DataConverter_PM.vhd
+++ firmware/common/gbt-readout/hdl/DataConverter_PM.vhd
@@ -27,9 +27,9 @@ entity DataConverter is
     Board_data_I : in board_data_type;
 
     header_fifo_data_o  : out std_logic_vector(GBT_data_word_bitdepth-1 downto 0);
-    data_fifo_data_o  : out std_logic_vector(GBT_data_word_bitdepth-1 downto 0);
+    data_fifo_data_o    : out std_logic_vector(GBT_data_word_bitdepth-1 downto 0);
     header_fifo_rden_i  : in  std_logic;
-    data_fifo_rden_i  : in  std_logic;
+    data_fifo_rden_i    : in  std_logic;
     header_fifo_empty_o : out std_logic;
 
     drop_ounter_o  : out std_logic_vector(15 downto 0);
@@ -46,44 +46,43 @@ architecture Behavioral of DataConverter is
       data_word => (others => '0')
       );
 
-  signal header_pcklen, header_pcklen_ff, header_pcklen_latch          : std_logic_vector(n_pckt_wrds_bitdepth-1 downto 0);
-  signal header_orbit           : std_logic_vector(Orbit_id_bitdepth-1 downto 0);
-  signal header_bc              : std_logic_vector(BC_id_bitdepth-1 downto 0);
-  signal header_word, header_word_latch, data_word : std_logic_vector(GBT_data_word_bitdepth-1 downto 0);
-  signal is_header, is_data     : std_logic;
+  signal header_pcklen, header_pcklen_ff, header_pcklen_latch : std_logic_vector(n_pckt_wrds_bitdepth-1 downto 0);
+  signal header_orbit                                         : std_logic_vector(Orbit_id_bitdepth-1 downto 0);
+  signal header_bc                                            : std_logic_vector(BC_id_bitdepth-1 downto 0);
+  signal header_word, header_word_latch, data_word            : std_logic_vector(GBT_data_word_bitdepth-1 downto 0);
+  signal is_header, is_data                                   : std_logic;
 
   signal send_mode_ison, send_mode_ison_sclk : boolean;
-  signal reset_drop_counters       : std_logic;
-  signal drop_counter              : std_logic_vector(15 downto 0);
+  signal reset_drop_counters                 : std_logic;
+  signal drop_counter                        : std_logic_vector(15 downto 0);
 
-  signal data_rawfifo_cnt, rawfifo_cnt_max : std_logic_vector(12 downto 0);
-  signal header_rawfifo_full, data_rawfifo_full, rawfifo_full                 : std_logic;
+  signal data_rawfifo_cnt, rawfifo_cnt_max                    : std_logic_vector(12 downto 0);
+  signal header_rawfifo_full, data_rawfifo_full, rawfifo_full : std_logic;
 
-  
-  signal sending_event, dropping_event : std_logic;
-  signal word_counter     : std_logic_vector(n_pckt_wrds_bitdepth-1 downto 0);
 
-  
-  signal header_fifo_din, data_fifo_din                     : std_logic_vector(GBT_data_word_bitdepth-1 downto 0);
-  signal header_fifo_we, data_fifo_we                       : std_logic;
+  signal sending_event : std_logic;
+  signal word_counter  : std_logic_vector(n_pckt_wrds_bitdepth-1 downto 0);
+
+
+  signal header_fifo_din, data_fifo_din : std_logic_vector(GBT_data_word_bitdepth-1 downto 0);
+  signal header_fifo_we, data_fifo_we   : std_logic;
 
 
   attribute keep                        : string;
   attribute keep of reset_drop_counters : signal is "true";
-  attribute keep of header_fifo_din           : signal is "true";
-  attribute keep of data_fifo_din             : signal is "true";
-  attribute keep of header_fifo_we             : signal is "true";
-  attribute keep of data_fifo_we             : signal is "true";
-  attribute keep of word_counter             : signal is "true";
-  attribute keep of sending_event             : signal is "true";
-  attribute keep of dropping_event             : signal is "true";
-  attribute keep of header_word             : signal is "true";
-  attribute keep of data_word             : signal is "true";
+  attribute keep of header_fifo_din     : signal is "true";
+  attribute keep of data_fifo_din       : signal is "true";
+  attribute keep of header_fifo_we      : signal is "true";
+  attribute keep of data_fifo_we        : signal is "true";
+  attribute keep of word_counter        : signal is "true";
+  attribute keep of sending_event       : signal is "true";
+  attribute keep of header_word         : signal is "true";
+  attribute keep of data_word           : signal is "true";
   attribute keep of is_data             : signal is "true";
-  attribute keep of is_header             : signal is "true";
-  attribute keep of header_pcklen_ff             : signal is "true";
-  attribute keep of header_word_latch             : signal is "true";
-  attribute keep of header_pcklen_latch             : signal is "true";
+  attribute keep of is_header           : signal is "true";
+  attribute keep of header_pcklen_ff    : signal is "true";
+  attribute keep of header_word_latch   : signal is "true";
+  attribute keep of header_pcklen_latch : signal is "true";
 
 begin
 
@@ -125,12 +124,12 @@ begin
       );
 ---- ===========================================================
 
-rawfifo_full <= header_rawfifo_full or data_rawfifo_full;
+  rawfifo_full <= header_rawfifo_full or data_rawfifo_full;
 
   process (FSM_Clocks_I.Data_Clk)
   begin
     if(rising_edge(FSM_Clocks_I.Data_Clk))then
-      send_mode_ison      <= (FIT_GBT_status_I.Readout_Mode /= mode_IDLE) or (Control_register_I.readout_bypass = '1');
+      send_mode_ison <= (FIT_GBT_status_I.Readout_Mode /= mode_IDLE) or (Control_register_I.readout_bypass = '1');
       drop_ounter_o  <= drop_counter;
       fifo_cnt_max_o <= "000"&rawfifo_cnt_max;
     end if;
@@ -144,11 +143,11 @@ rawfifo_full <= header_rawfifo_full or data_rawfifo_full;
 
       reset_drop_counters <= Control_register_I.reset_drophit_counter;
 
-	  header_word <= func_FITDATAHD_get_header(header_pcklen, header_orbit, header_bc, FIT_GBT_status_I.rx_phase, FIT_GBT_status_I.GBT_status.Rx_Phase_error, '0');
-      data_word   <= Board_data_I.data_word;
-      is_data     <= Board_data_I.is_data;
-      is_header   <= Board_data_I.is_header;
-	  header_pcklen_ff <= header_pcklen;
+      header_word      <= func_FITDATAHD_get_header(header_pcklen, header_orbit, header_bc, FIT_GBT_status_I.rx_phase, FIT_GBT_status_I.GBT_status.Rx_Phase_error, '0');
+      data_word        <= Board_data_I.data_word;
+      is_data          <= Board_data_I.is_data;
+      is_header        <= Board_data_I.is_header;
+      header_pcklen_ff <= header_pcklen;
 
       send_mode_ison_sclk <= send_mode_ison;
 
@@ -157,37 +156,37 @@ rawfifo_full <= header_rawfifo_full or data_rawfifo_full;
         sending_event   <= '0';
         drop_counter    <= (others => '0');
         rawfifo_cnt_max <= (others => '0');
-		word_counter <= (others => '0');
+        word_counter    <= (others => '0');
 
       else
 
         if is_header = '1' then
-		
-		  header_word_latch <= header_word;
-		  header_pcklen_latch <= header_pcklen_ff;
-		  word_counter <= (others => '0');
-		
+
+          header_word_latch   <= header_word;
+          header_pcklen_latch <= header_pcklen_ff;
+          word_counter        <= (others => '0');
+
           if (rawfifo_full = '0') and send_mode_ison_sclk then
             sending_event <= '1';
           else
             sending_event <= '0';
           end if;
-		  
-		  if (rawfifo_full = '1') and send_mode_ison_sclk then
-            drop_counter  <= drop_counter + 1;
+
+          if (rawfifo_full = '1') and send_mode_ison_sclk then
+            drop_counter <= drop_counter + 1;
           end if;
-		  
-		elsif is_data = '1' then
-		  
-		  word_counter <= word_counter + 1;
-		  
-		else
-		
-		  word_counter <= (others => '0');
-		  
+
+        elsif is_data = '1' then
+
+          word_counter <= word_counter + 1;
+
+        else
+
+          word_counter <= (others => '0');
+
         end if;
-		
-		
+
+
 
         if rawfifo_cnt_max < data_rawfifo_cnt then rawfifo_cnt_max <= data_rawfifo_cnt; end if;
 
@@ -206,10 +205,10 @@ rawfifo_full <= header_rawfifo_full or data_rawfifo_full;
 -- ****************************************************
 
   header_fifo_din <= header_word_latch;
-  header_fifo_we <= '1' when word_counter = header_pcklen_latch and sending_event = '1' else '0';
-  
+  header_fifo_we  <= '1' when word_counter = header_pcklen_latch and sending_event = '1' else '0';
+
   data_fifo_din <= data_word;
-  data_fifo_we <= '1' when is_data = '1' and is_header = '0' and sending_event = '1' else '0';
+  data_fifo_we  <= '1' when is_data = '1' and is_header = '0' and sending_event = '1' else '0';
 
 end Behavioral;
 
